8cf37bede1209d4060c6f79d409a61ba4e9329d4
Chethan Kumblekar <chethan.kumblekar@ascertra.com>
2024-04-14 00:18:43 +0530

    Add project files.

---
+++ DataService/SharedDb.cs
@@ -0,0 +1,11 @@
+﻿using PlayGround.ChatService.Models;
+using System.Collections.Concurrent;
+
+namespace PlayGround.ChatService.DataService
+{
+    public class SharedDb
+    {
+        private readonly ConcurrentDictionary<string, UserConnection> _connections = new ConcurrentDictionary<string, UserConnection>();
+        public ConcurrentDictionary<string,UserConnection> connections => _connections;
+    }
+}
---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using PlayGround.ChatService.DataService;
+using PlayGround.ChatService.Models;
+
+namespace PlayGround.ChatService.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly SharedDb _sharedDb;
+        
+        public ChatHub(SharedDb sharedDb) => _sharedDb = sharedDb;
+
+        public async Task JoinChat(UserConnection userConnection)
+        {
+            await Clients.All.SendAsync("RecieveMessage", "admin", $"{userConnection.UserName} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.ChatRoom);
+            
+            _sharedDb.connections[Context.ConnectionId] = userConnection;
+
+            await Clients.Group(userConnection.ChatRoom)
+                .SendAsync("RecieveSpecificMessage", "admin", $"{userConnection.UserName} has joined {userConnection.ChatRoom}");
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if(_sharedDb.connections.TryGetValue(Context.ConnectionId,out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.ChatRoom)
+                    .SendAsync("RecieveSpecificMessage", userConnection.UserName, $"{message}");
+            }
+        }
+
+    }
+}
